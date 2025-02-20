31efc193ce596f5971e7aa0bd6cea3ee48e571ac
andybhattacharyya <bhattacharyya.anand@gmail.com>
2025-02-19 19:56:34 -0500

    Utilized try with resources construct to safely closed resources after image is transferred

--- FileIO/Main.java
+++ FileIO/Main.java
@@ -7,16 +7,18 @@ import java.net.Socket;
 public class Main {
     public static void main(String[] args) throws IOException {
         System.out.println("Current Directory: " + System.getProperty("user.dir"));
+        try(
         ServerSocket fs = new ServerSocket(8080);
         Socket client = fs.accept();
         InputStream cin = client.getInputStream();
         FileOutputStream fout = new FileOutputStream("dtest.png");
-        System.out.println("This much data to copy: " + cin.available());
-        while(cin.available() > 0) {
-            fout.write(cin.read());
-            //not sure if this is necessarry
-            fout.flush();
-        }
-        fout.close();
+        ) {
+            System.out.println("This much data to copy: " + cin.available());
+            while (cin.available() > 0) {
+                fout.write(cin.read());
+                //not sure if this is necessarry
+                fout.flush();
+            }
+        } catch (IOException e) {System.out.println("error");}
     }
 }
